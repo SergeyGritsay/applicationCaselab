81fedc1dc3ddcfab42269cbe8fefe89a3c513904
SergeyGritsay <darknezz31ru@gmail.com>
2020-07-16 17:59:21 +0300

    First Commit.

---
+++ models/user.js
@@ -0,0 +1,28 @@
+//import mongoose, {Schema} from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+	login: {type: String, unique: true, lowercase: true},
+	password: String
+});
+
+UserSchema.pre('save', async function(next) {
+	if (!this.isModified('password')){
+		return next();
+	}
+
+	const salt = await bcrypt.genSalt(10);
+	const hash = await bcrypt.hash(this.password, salt);
+
+	this.password = hash;
+	next();
+});
+
+UserSchema.methods.comparePasswords = function(password) {
+	return bcrypt.compare(password, this.password);
+};
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
